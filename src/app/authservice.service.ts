a67921c9a9627ca434fb4e586364cf7b187bb1f0
shalimov <shalimov@profiterole.group>
2025-07-19 15:50:40 +0300

    fix login

--- src/app/authservice.service.ts
+++ src/app/authservice.service.ts
@@ -1,22 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
+const API_URL = 'http://localhost:8080/auth'; // <-- указываем URL бэкенда
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthserviceService {
-
   constructor(private http: HttpClient) {}
+
   login(credentials: { email: string, password: string }) {
     const form = new FormData();
-    form.append('username', credentials.email);
+    form.append('email', credentials.email);
     form.append('password', credentials.password);
-    return this.http.post('/api/login', form);
+
+    return this.http.post<{ token: string }>(`${API_URL}/login`, {
+  email: credentials.email,
+  password: credentials.password
+})
+      .pipe(
+        tap(response => {
+          localStorage.setItem('auth_token', response.token);
+        })
+      );
   }
 
   register(data: { email: string, password: string }) {
-    return this.http.post('/api/register', data);
+    return this.http.post(`${API_URL}/api/register`, data);
   }
-  
-
 }

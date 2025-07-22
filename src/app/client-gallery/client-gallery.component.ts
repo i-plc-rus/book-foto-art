f8602fd781c0039b7bb28bae4d258ed980efd4ab
shalimov <shalimov@profiterole.group>
2025-07-22 15:48:44 +0300

    upd wizart create collenction

--- src/app/client-gallery/client-gallery.component.ts
+++ src/app/client-gallery/client-gallery.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-gallery',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './client-gallery.component.html',
   styleUrl: './client-gallery.component.css'
 })
 export class ClientGalleryComponent {
+  currentStep = 1;
 
+  galleryName = '';
+  galleryDate = '';
+
+  nextStep() {
+    this.currentStep++;
+  }
 }
---
+++ src/app/gallery-upload/gallery-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-upload',
+  imports: [],
+  templateUrl: './gallery-upload.component.html',
+  styleUrl: './gallery-upload.component.css'
+})
+export class GalleryUploadComponent {
+
+}

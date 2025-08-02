8db4c0d831b9aeee63f80d44fac96e1339c2cff6
NursultanFront <nursultan020395@gmail.com>
2025-08-02 18:24:27 +0500

    feat: make gallery image card

---
+++ src/app/module/collection-site/gallery-image-card/gallery-image-card.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Collection = {
+  link: string;
+  isFavorite: boolean;
+};
+
+@Component({
+  selector: 'app-gallery-image-card',
+  imports: [CommonModule],
+  templateUrl: './gallery-image-card.component.html',
+  styleUrls: ['./gallery-image-card.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GalleryImageCardComponent {
+  readonly imageUrl = input.required<Collection>();
+
+  downloadImage(): void {
+    const link = document.createElement('a');
+    link.href = this.imageUrl().link;
+    link.download = this.extractFileName(this.imageUrl().link);
+    link.click();
+  }
+
+  private extractFileName(url: string): string {
+    return url.split('/').pop() || 'image.jpg';
+  }
+}
--- src/app/pages/collection-site/collection-site.component.ts
+++ src/app/pages/collection-site/collection-site.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GalleryImageCardComponent } from '../../module/collection-site/gallery-image-card/gallery-image-card.component';
 
 interface ISavedGallery {
   name: string;
@@ -26,24 +27,52 @@ export const mockGalleries: ISavedGallery = {
       isFavorite: true,
     },
     {
-      link: 'assets/mockImages/42a4824f7f9b0ae93d9a6bd27f64927.jpg',
-      isFavorite: false,
-    },
-    {
       link: 'assets/mockImages/1702895570_bogatyr-club-p-koshechka-iz-toma-i-dzherri-foni-instagram-4.png',
       isFavorite: true,
-    },
-    {
-      link: 'assets/mockImages/c63614620afcdef67c6a6e32d1d557a9.jpg',
-      isFavorite: false,
     },
     {
       link: 'assets/mockImages/ead3b558586cc51653230d9eaf58f503.jpg',
       isFavorite: true,
     },
     {
-      link: 'assets/mockImages/ecfeae202fa63b267b9ab14f051828b6.jpg',
+      link: 'assets/mockImages/f4319ba238bdc7da00c56adbb45461dc.jpg',
       isFavorite: false,
+    },
+    {
+      link: 'assets/mockImages/8dfa455df6206f75c8bee021fdeca0b9.jpg',
+      isFavorite: false,
+    },
+    {
+      link: 'assets/mockImages/7ca83dc4f5e31627917e87ac7c6ba77d.jpg',
+      isFavorite: true,
+    },
+    {
+      link: 'assets/mockImages/1702895570_bogatyr-club-p-koshechka-iz-toma-i-dzherri-foni-instagram-4.png',
+      isFavorite: true,
+    },
+    {
+      link: 'assets/mockImages/ead3b558586cc51653230d9eaf58f503.jpg',
+      isFavorite: true,
+    },
+    {
+      link: 'assets/mockImages/f4319ba238bdc7da00c56adbb45461dc.jpg',
+      isFavorite: false,
+    },
+    {
+      link: 'assets/mockImages/8dfa455df6206f75c8bee021fdeca0b9.jpg',
+      isFavorite: false,
+    },
+    {
+      link: 'assets/mockImages/7ca83dc4f5e31627917e87ac7c6ba77d.jpg',
+      isFavorite: true,
+    },
+    {
+      link: 'assets/mockImages/1702895570_bogatyr-club-p-koshechka-iz-toma-i-dzherri-foni-instagram-4.png',
+      isFavorite: true,
+    },
+    {
+      link: 'assets/mockImages/ead3b558586cc51653230d9eaf58f503.jpg',
+      isFavorite: true,
     },
     {
       link: 'assets/mockImages/f4319ba238bdc7da00c56adbb45461dc.jpg',
@@ -56,7 +85,7 @@ export const mockGalleries: ISavedGallery = {
   selector: 'app-collection-site',
   templateUrl: './collection-site.component.html',
   styleUrls: ['./collection-site.component.scss'],
-  imports: [DatePipe, RouterLink],
+  imports: [DatePipe, RouterLink, GalleryImageCardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })

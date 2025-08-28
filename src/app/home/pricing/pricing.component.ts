595c380c1ba4c37b8294a052ad00e689cf2d2b26
Pavel Pogorelov <engineer.pavel@ya.ru>
2025-08-28 14:42:13 +0300

    pricing

---
+++ src/app/home/pricing/pricing.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing',
+  imports: [],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PricingComponent {}

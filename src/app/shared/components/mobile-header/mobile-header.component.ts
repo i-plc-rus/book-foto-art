ffcc69531e14a7097712e2e44246af1f18e6b362
Pavel Pogorelov <engineer.pavel@ya.ru>
2025-09-01 16:55:31 +0300

    копирование ссылки в мобильной версии

--- src/app/shared/components/mobile-header/mobile-header.component.ts
+++ src/app/shared/components/mobile-header/mobile-header.component.ts
@@ -1,31 +1,69 @@
 import { Component, computed, inject, Input, signal } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { MobileMenuSheetComponent } from '../mobile-menu-sheet/mobile-menu-sheet.component';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+
+import { MobileMenuSheetComponent } from '../mobile-menu-sheet/mobile-menu-sheet.component';
 
 @Component({
   standalone: true,
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
   styleUrl: './mobile-header.component.css',
-  imports: [],
+  imports: [Toast],
 })
 export class MobileHeaderComponent {
   @Input() titleSignal = signal<any | null>(null);
+  @Input() isPublished: boolean = false;
+  @Input() publicLink: string | null = null;
+
+  private readonly messageService = inject(MessageService);
 
   title = computed(() => this.titleSignal()?.name ?? '');
   private bottomSheet = inject(MatBottomSheet);
   private router = inject(Router);
 
-  goBack() {
-    this.router.navigate(['/client-gallery']);
+  async goBack(): Promise<void> {
+    await this.router.navigate(['/client-gallery']);
   }
 
-  onEdit() {
+  onEdit(): void {
     console.log('Edit clicked');
   }
 
-  openMenu() {
+  openMenu(): void {
     this.bottomSheet.open(MobileMenuSheetComponent);
+  }
+
+  async copyPublicLink(): Promise<void> {
+    if (!this.publicLink) {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Нет ссылки',
+        detail: 'Коллекция не опубликована',
+        life: 2000,
+      });
+      return;
+    }
+
+    try {
+      await navigator.clipboard.writeText(this.publicLink);
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Скопировано',
+        detail: this.publicLink,
+        life: 1500,
+      });
+    } catch (e) {
+      console.error('Clipboard error', e);
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Ошибка',
+        detail: 'Не удалось скопировать ссылку',
+        life: 2500,
+      });
+    }
   }
 }
